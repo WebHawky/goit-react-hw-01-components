1f819fea7d8e01dad5cf058b428862292a25eb08
Bohdan <102989300+WebHawky@users.noreply.github.com>
2022-08-13 18:33:04 +0300

    done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -30,7 +30,7 @@ export default function Profile({ username, tag, location, avatar, stats }) {
 }
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
+  // username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
